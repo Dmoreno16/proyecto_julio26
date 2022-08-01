f93d3d2cf8542bbf5855b43d78806168661b74eb
dmoreno14 <dmoreno@digitalitglobal.com>
2022-08-01 11:09:23 -0500

    cambios en gitignore

--- project/settings.py
+++ project/settings.py
@@ -25,6 +25,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
