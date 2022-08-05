b9cb48097ccea25cf316033350ac23d0f59852a9
dmoreno14 <dmoreno@digitalitglobal.com>
2022-08-05 08:52:39 -0500

    creacion de tablas base de datos

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.forms import DateTimeField
 
 # Create your models here.
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_data=models.DateTimeField('data_published')
+
+
+
